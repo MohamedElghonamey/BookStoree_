acbb3f47d263262e042214b760672b433b4ccdac
Mohamed <mohamedelghonamey500@hotmail.com>
2022-08-26 23:15:13 +0200

    Add project files.

---
+++ BookStore_/Models/Repsotrey/AuthorRepostory.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models.Repsotrey
+{
+    public class AuthorRepostory : IBookStoreRepostery<Author>
+    {
+        IList<Author> authors;
+        public AuthorRepostory()
+        {
+            authors = new List<Author>()
+            {
+                new Author{ID=1, FullName = "Mohamed Mostafa "},
+                  new Author{ID=2, FullName = "Hesham Ahmed "},
+                    new Author{ID=3, FullName = "Mahmoud Ahmed "}
+            };
+        }
+        public void Add(Author entity)
+        {
+            authors.Add(entity);    
+        }
+
+        public void Delete(int ID)
+        {
+            var author = Find(ID);
+            authors.Remove(author); 
+        }
+
+        public Author Find(int ID)
+        {
+            var author = authors.SingleOrDefault(a => a.ID == ID);
+            return author;
+        }
+
+        public IList<Author> List()
+        {
+            return authors;
+        }
+
+        public void Update(int id, Author newAuthor)
+        {
+           var author = Find(id);
+            author.FullName= newAuthor.FullName;    
+        }
+    }
+}
---
+++ BookStore_/Models/Repsotrey/IBookStoreRepostery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models.Repsotrey
+{
+    public interface IBookStoreRepostery<TEntity>
+    {
+        IList<TEntity> List();
+        TEntity Find(int ID); 
+        void Add(TEntity entity);   
+        void Update(int id,TEntity entity);    
+        void Delete(int ID);    
+
+
+    }
+}
