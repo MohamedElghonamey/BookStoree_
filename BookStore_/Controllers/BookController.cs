5703360bc08e4860b3083f06d98ef32433687956
Mohamed <mohamedelghonamey500@hotmail.com>
2022-08-26 23:15:13 +0200

    Add project files.

---
+++ BookStore_/Controllers/BookController.cs
@@ -0,0 +1,142 @@
+﻿using BookStore.Models;
+using BookStore.Models.Repsotrey;
+using BookStore.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly IBookStoreRepostery<Book> bookRepostery;
+        private readonly IBookStoreRepostery<Author> authorRepostory;
+
+        public BookController(IBookStoreRepostery<Book> bookRepostery , 
+             IBookStoreRepostery<Author> authorRepostory)
+        {
+            this.bookRepostery = bookRepostery;
+            this.authorRepostory = authorRepostory;
+        }
+        // GET: BookController
+        public ActionResult Index()
+        {
+            var books = bookRepostery.List();
+            return View(books);
+        }
+
+        // GET: BookController/Details/5
+        public ActionResult Details(int id)
+        {
+            var book= bookRepostery.Find(id);
+            return View(book);
+        }
+
+        // GET: BookController/Create
+        public ActionResult Create()
+        {
+            var model = new BookAuthorViewModel
+        {
+                Authors=authorRepostory.List().ToList()
+           
+           };
+            return View(model);
+        }
+
+        // POST: BookController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(BookAuthorViewModel model)
+        {
+            try
+
+            {
+                var Author = authorRepostory.Find(model.AuthorId);
+                Book book = new Book
+                {
+                    ID= model.BookId, 
+                    Discription= model.Discription,
+                    Title= model.Title,
+                    Author= Author
+
+                };
+                bookRepostery.Add(book);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BookController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var book = bookRepostery.Find(id);
+            var AuthorId = book.Author == null ? book.Author.ID = 0 : book.Author.ID; 
+            var viewModel = new BookAuthorViewModel
+            {
+                BookId= book.ID,
+                Title=book.Title,
+                Discription=book.Discription,
+                AuthorId= book.Author.ID,
+                Authors=authorRepostory.List().ToList()
+            };
+            return View(viewModel);
+        }
+
+        // POST: BookController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, BookAuthorViewModel viewModel)
+        {
+            try
+            {
+                var Authors = authorRepostory.Find(viewModel.AuthorId);
+                Book book = new Book
+                {
+                   
+                    Discription = viewModel.Discription,
+                    Title = viewModel.Title,
+                    Author = Authors
+
+                };
+                bookRepostery.Update(viewModel.BookId, book);
+                return RedirectToAction(nameof(Edit));
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BookController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var Book = bookRepostery.Find(id);
+            
+            return View(Book);
+        }
+
+        // POST: BookController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ConfirmDelete(int id)
+        {
+            try
+            {       
+                bookRepostery.Delete(id);              
+                return RedirectToAction(nameof(Index));
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BookStore_/Models/Repsotrey/BookRepostory.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models.Repsotrey
+{
+    public class BookRepostory : IBookStoreRepostery<Book>
+    {
+        List<Book> books;
+
+        public BookRepostory()
+        {
+            books = new List<Book>()
+              {
+                  new Book
+                  {
+                      ID =1,
+                      Title="C#",
+                      Discription="Programming Lang",
+                        Author=new Author()
+                  },
+                   new Book
+                  {
+                      ID =2,
+                      Title="C++",
+                      Discription="Programming Lang1",
+                      Author=new Author()
+                  },
+                    new Book
+                  {
+                      ID =3,
+                      Title="Java",
+                      Discription="Programming ",
+                        Author=new Author()
+                  }
+              };
+            
+        }
+        public void Add(Book entity)
+        {
+            books.Add(entity);    
+        }
+
+        public void Delete(int ID)
+        {
+     var book=books.SingleOrDefault(b=>b.ID==ID);
+            books.Remove(book);
+        }
+
+        public Book Find(int id)
+        {
+            var  book = books.SingleOrDefault(b => b.ID == id );
+            return book;
+        }
+
+        public IList<Book> List()
+        {
+            return books;
+        }
+
+        public void Update(int id, Book NewBooks)
+        {
+            var book =Find(id);
+            book.Author = NewBooks.Author;
+            book.Title=NewBooks.Title;  
+            book.Discription=NewBooks.Discription;  
+
+        }
+
+        
+    }
+}
